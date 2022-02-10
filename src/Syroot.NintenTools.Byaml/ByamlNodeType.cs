0272d4633ef40c75040000eef5ac5f77580afc37
OatmealDome <julian@oatmealdome.me>
2022-02-10 16:30:58 -0500

    Serialization: Remove support

--- src/Syroot.NintenTools.Byaml/ByamlNodeType.cs
+++ src/Syroot.NintenTools.Byaml/ByamlNodeType.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace OatmealDome.NinLib.Byaml
@@ -85,52 +85,5 @@ namespace OatmealDome.NinLib.Byaml
         /// The node represents <c>null</c>.
         /// </summary>
         Null = 0xFF
-    }
-
-    /// <summary>
-    /// Represents extension methods for <see cref="ByamlNodeType"/> instances.
-    /// </summary>
-    internal static class ByamlNodeTypeExtensions
-    {
-        /// <summary>
-        /// Gets the corresponding, instantiatable <see cref="Type"/> for the given <paramref name="nodeType"/>.
-        /// </summary>
-        /// <param name="nodeType">The <see cref="ByamlNodeType"/> which should be instantiated.</param>
-        /// <returns>The <see cref="Type"/> to instantiate for the node.</returns>
-        internal static Type GetInstanceType(this ByamlNodeType nodeType, ByamlVersion version)
-        {
-            switch (nodeType)
-            {
-                case ByamlNodeType.StringIndex:
-                    return typeof(string);
-                case ByamlNodeType.PathIndex:
-                    if (version == ByamlVersion.Four)
-                    {
-                        return typeof(byte[]);
-                    }
-                    else if (version == ByamlVersion.One)
-                    {
-                        return typeof(List<ByamlPathPoint>);
-                    }
-
-                    throw new ByamlException($"Invalid node type {nodeType} for version {version}.");
-                case ByamlNodeType.Array:
-                    // TODO: Check if this could be loaded as an object array.
-                    throw new ByamlException("Cannot instantiate an array of unknown element type.");
-                case ByamlNodeType.Dictionary:
-                    // TODO: Check if this could be loaded as a string-object dictionary.
-                    throw new ByamlException("Cannot instantiate an object of unknown type.");
-                case ByamlNodeType.Boolean:
-                    return typeof(bool);
-                case ByamlNodeType.Integer:
-                    return typeof(int);
-                case ByamlNodeType.Float:
-                    return typeof(float);
-                case ByamlNodeType.Null:
-                    return typeof(object);
-                default:
-                    throw new ByamlException($"Unknown node type {nodeType}.");
-            }
-        }
     }
 }
